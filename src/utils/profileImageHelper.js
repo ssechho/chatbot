4ed79dcf170cc60fdf6f10a7d88341b1a57e7a93
ssunny0208 <xdfv635@snu.ac.kr>
2024-05-30 20:43:29 +0900

    profile edit4

--- src/utils/profileImageHelper.js
+++ src/utils/profileImageHelper.js
@@ -1,36 +1,44 @@
 const profileImages = {
-  intellectual: [
-    "/images/profile_intellectual/boy_0.png",
-    "/images/profile_intellectual/boy_1.png",
-    "/images/profile_intellectual/boy_2.png",
-    "/images/profile_intellectual/boy_3.png",
-    "/images/profile_intellectual/boy_4.png",
-    "/images/profile_intellectual/boy_5.png",
-    "/images/profile_intellectual/boy_6.png",
-    "/images/profile_intellectual/girl_0.png",
-    "/images/profile_intellectual/girl_1.png",
-    "/images/profile_intellectual/girl_2.png",
-    "/images/profile_intellectual/girl_3.png",
-    "/images/profile_intellectual/girl_4.png",
-    "/images/profile_intellectual/girl_5.png",
-    "/images/profile_intellectual/girl_6.png",
-  ],
-  funny: [
-    "/images/profile_funny/boy_0.png",
-    "/images/profile_funny/boy_1.png",
-    "/images/profile_funny/boy_2.png",
-    "/images/profile_funny/boy_3.png",
-    "/images/profile_funny/boy_4.png",
-    "/images/profile_funny/boy_5.png",
-    "/images/profile_funny/boy_6.png",
-    "/images/profile_funny/girl_0.png",
-    "/images/profile_funny/girl_1.png",
-    "/images/profile_funny/girl_2.png",
-    "/images/profile_funny/girl_3.png",
-    "/images/profile_funny/girl_4.png",
-    "/images/profile_funny/girl_5.png",
-    "/images/profile_funny/girl_6.png",
-  ],
+  intellectual: {
+    boy: [
+      "/images/profile_intellectual/boy_0.png",
+      "/images/profile_intellectual/boy_1.png",
+      "/images/profile_intellectual/boy_2.png",
+      "/images/profile_intellectual/boy_3.png",
+      "/images/profile_intellectual/boy_4.png",
+      "/images/profile_intellectual/boy_5.png",
+      "/images/profile_intellectual/boy_6.png",
+    ],
+    girl: [
+      "/images/profile_intellectual/girl_0.png",
+      "/images/profile_intellectual/girl_1.png",
+      "/images/profile_intellectual/girl_2.png",
+      "/images/profile_intellectual/girl_3.png",
+      "/images/profile_intellectual/girl_4.png",
+      "/images/profile_intellectual/girl_5.png",
+      "/images/profile_intellectual/girl_6.png",
+    ],
+  },
+  funny: {
+    boy: [
+      "/images/profile_funny/boy_0.png",
+      "/images/profile_funny/boy_1.png",
+      "/images/profile_funny/boy_2.png",
+      "/images/profile_funny/boy_3.png",
+      "/images/profile_funny/boy_4.png",
+      "/images/profile_funny/boy_5.png",
+      "/images/profile_funny/boy_6.png",
+    ],
+    girl: [
+      "/images/profile_funny/girl_0.png",
+      "/images/profile_funny/girl_1.png",
+      "/images/profile_funny/girl_2.png",
+      "/images/profile_funny/girl_3.png",
+      "/images/profile_funny/girl_4.png",
+      "/images/profile_funny/girl_5.png",
+      "/images/profile_funny/girl_6.png",
+    ],
+  },
   intellectual_default: {
     boy: "/images/profile_intellectual/boy_default.png",
     girl: "/images/profile_intellectual/girl_default.png",
@@ -42,17 +50,9 @@ const profileImages = {
 };
 
 export const getProfileImage = (index, defaultProfile, mode) => {
-  const defaultImageSet = profileImages[`${mode}_default`];
   if (index === 0) {
-    if (defaultImageSet && defaultImageSet[defaultProfile]) {
-      return defaultImageSet[defaultProfile];
-    } else {
-      return "/images/default_profile.png"; // 기본 이미지가 없을 경우 대비
-    }
+    return profileImages[`${mode}_default`][defaultProfile];
   }
-  const profileOptions = profileImages[mode];
-  if (profileOptions) {
-    return profileOptions[(index - 1) % profileOptions.length];
-  }
-  return "/images/default_profile.png"; // 기본 이미지가 없을 경우 대비
+  const profileOptions = profileImages[mode][defaultProfile];
+  return profileOptions[(index - 1) % profileOptions.length];
 };
