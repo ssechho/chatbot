63cb458aedfd5181c38fc44ca3db764a308b88ea
ssunny0208 <xdfv635@snu.ac.kr>
2024-06-03 13:53:25 +0900

    Revert "save profile"
    This reverts commit fde33659becc894a2276c96b021d8f76b7d17da2.

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ChatInput } from "./ChatInput";
 import { ChatLoader } from "./ChatLoader";
 import { ChatBubble } from "./ChatBubble";
