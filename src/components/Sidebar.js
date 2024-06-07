61651ab4c8dc53fbe13d6d4b3169ab2b16e4bee1
Seoee Choi <142161282+ssechho@users.noreply.github.com>
2024-06-07 21:55:42 +0900

    delete icon fix

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -29,31 +29,31 @@ const Sidebar = ({
         New Conversation
       </button>
       <div className="flex-1 overflow-y-auto">
-      <ul>
-        {conversations.map((conversation, index) => (
-          <li
-            key={index}
-            className="p-2 mb-2 ml-4 flex justify-between items-center cursor-pointer hover:bg-neutral-900"
-            onClick={() => handleSelectConversation(conversation.id)} // ID를 전달
-          >
-            <span className="text-white">{conversation.title}</span>
-            <button
-              className="text-red-500 ml-2"
-              onClick={(e) => {
-                e.stopPropagation(); // 클릭 이벤트가 상위 요소로 전파되지 않도록 함
-                onDeleteConversation(conversation.id);
-              }}
+        <ul>
+          {conversations.map((conversation, index) => (
+            <li
+              key={index}
+              className="p-2 mb-2 ml-4 flex justify-between items-center cursor-pointer hover:bg-neutral-900"
+              onClick={() => handleSelectConversation(conversation.id)} // ID를 전달
             >
-              <img src="/images/deleteIcon.svg" alt="Delete" />
-            </button>
-          </li>
-        ))}
-      </ul>
-      {loading && (
-        <div className="flex justify-center items-center h-full">
-          <div className="loader"></div>
-        </div>
-      )}
+              <span className="text-white">{conversation.title}</span>
+              <button
+                className="text-red-500 ml-2 w-6 h-6 flex justify-center items-center"
+                onClick={(e) => {
+                  e.stopPropagation(); // 클릭 이벤트가 상위 요소로 전파되지 않도록 함
+                  onDeleteConversation(conversation.id);
+                }}
+              >
+                <img src="/images/deleteIcon.svg" alt="Delete" className="w-4 h-4" />
+              </button>
+            </li>
+          ))}
+        </ul>
+        {loading && (
+          <div className="flex justify-center items-center h-full">
+            <div className="loader"></div>
+          </div>
+        )}
       </div>
     </div>
   );
