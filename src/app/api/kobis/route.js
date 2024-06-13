d204c159d18fcf4c8ed28a69187eb9aeb878ffb2
ssunny0208 <xdfv635@snu.ac.kr>
2024-06-13 10:24:44 +0900

    Update route.js

--- src/app/api/kobis/route.js
+++ src/app/api/kobis/route.js
@@ -15,10 +15,12 @@ export async function POST(req) {
     const data = await req.json();
     const { query, movieList } = data;
 
-    const prompt = `다음 영화 목록에서 "${query}"와 가장 일치하는 영화를 선택하고 해당 영화의 movieCd를 반환하세요. 가장 일치하는 영화가 여러 개라면, 그 중 첫번째 영화의 movieCd를 반환하세요. 반드시 한 개의 movieCd는 반환해야 합니다:\n\n${movieList.map(movie => `Title: ${movie.movieNm}, movieCd: ${movie.movieCd}`).join('\n')}\n\n가장 일치하는 영화의 movieCd는:`;
+    const prompt = `다음 영화 목록에서 "${query}"와 가장 일치하는 영화를 선택하고 해당 영화의 movieCd를 반환하세요. 가장 일치하는 영화가 여러 개라면, 그 중 첫번째 영화의 movieCd를 반환하세요. 반드시 한 개의 movieCd는 반환해야 합니다:\n\n${movieList
+      .map((movie) => `Title: ${movie.movieNm}, movieCd: ${movie.movieCd}`)
+      .join("\n")}\n\n가장 일치하는 영화의 movieCd는:`;
 
     const response = await openai.chat.completions.create({
-      model: 'gpt-4',
+      model: "gpt-4o",
       messages: [
         { role: "system", content: "You are a helpful assistant." },
         { role: "user", content: prompt },
@@ -32,8 +34,11 @@ export async function POST(req) {
     });
   } catch (error) {
     console.error("Error fetching best match:", error);
-    return new Response(JSON.stringify({ message: "Error fetching best match" }), {
-      status: 500,
-    });
+    return new Response(
+      JSON.stringify({ message: "Error fetching best match" }),
+      {
+        status: 500,
+      }
+    );
   }
 }
