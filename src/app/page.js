e48d89230825b61c17dece816e1f5df141a04175
Kim Dongwhee <90066809+Dongwhee-1@users.noreply.github.com>
2024-06-14 22:02:02 +0900

    final fix I hope

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,17 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { Suspense } from "react";
 import Chatbot from "@/components/Chatbot";
 
 export default function Home() {
   return (
-    <SessionProvider>
-      <div>
-        <Chatbot />
-      </div>
-    </SessionProvider>
+    <Suspense fallback={<div>Loading...</div>}>
+      <SessionProvider>
+        <div>
+          <Chatbot />
+        </div>
+      </SessionProvider>
+    </Suspense>
   );
 }
